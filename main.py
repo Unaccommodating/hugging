9b26fc5e2576a57ecf8c82a037282f9b4c5f0d26
Unaccommodating <stud0000225549@study.utmn.ru>
2023-12-21 20:12:28 +0500

    FastAPI + sentiment-analysis

---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+
+# Import the necessary modules from transformers
+from transformers import pipeline
+
+app = FastAPI()
+
+# Load the sentiment analysis models
+sentiment_models = {
+    "baseline": "blanchefort/rubert-base-cased-sentiment",
+    "medical": "blanchefort/rubert-base-cased-sentiment-med",
+    "rusentiment": "blanchefort/rubert-base-cased-sentiment-rusentiment",
+    "rureviews": "blanchefort/rubert-base-cased-sentiment-rurewiews",
+    "social_media": "blanchefort/rubert-base-cased-sentiment-mokoron",
+}
+
+# Define the pipeline for each model
+pipelines = {name: pipeline("sentiment-analysis", model) for name, model in sentiment_models.items()}
+
+# Create an endpoint for sentiment analysis
+@app.post("/analyze-sentiment/{model_name}")
+async def analyze_sentiment(model_name: str, text: str):
+    # Check if the specified model exists
+    if model_name not in pipelines:
+        return {"error": f"Model '{model_name}' not found."}
+
+    # Perform sentiment analysis using the specified model
+    classifier = pipelines[model_name]
+    result = classifier(text)
+
+    # Return the sentiment analysis result
+    return {"model": model_name, "text": text, "sentiment": result[0]}
+
+# Run the FastAPI app
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
